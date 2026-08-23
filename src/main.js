07b19d5c3b90dd3ac2568ce02eefa364a7f8e287
Poya Faraji <poya.faraji@outlook.com>
2026-08-23 23:31:49 +0330

    [ADD] one one my old project back to life when I was learning tailwind

---
+++ src/main.js
@@ -0,0 +1,12 @@
+const navBtn = document.getElementById("menu");
+const menuBar = document.getElementById("menubar");
+
+navBtn.addEventListener("click", () => {
+  // for Screen readers
+  const isExpanded = JSON.parse(navBtn.getAttribute("aria-expanded"));
+  navBtn.setAttribute("aria-expanded", !isExpanded);
+
+  menuBar.classList.toggle("hidden");
+  menuBar.classList.toggle("flex");
+  console.log("W");
+});
